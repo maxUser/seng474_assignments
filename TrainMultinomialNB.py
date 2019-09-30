2e236fd19a26e3ea272da05a47b569e3d3f137b1
Nikola Rados <nikola.rados@s-sm.org>
2019-09-30 11:59:13 -0700

    Rewrite and make fixes

--- TrainMultinomialNB.py
+++ TrainMultinomialNB.py
@@ -1,75 +1,142 @@
-def main():
-    # C = all classes; c = one class
-    # D = entire document; d = one line
-    # V = vocabulary
-    # N = number of lines?
-    train_data = open("traindata.txt", "r")
-    train_labels = open("trainlabels.txt", "r")
-
-    # Classes
-    # 0 = wise
-    # 1 = future
-    classes = (0, 1)
-    vocab = []
-    num_lines = 0
-    docs_in_class = 0
-    prior_c = []
-    count = 0
-    future_phrases = []
-    wise_phrases = []
-
-    for line in train_data:
-
-        split_line = line.split()
-        for word in split_line:
-            if word not in vocab:
-                vocab.append(word) # V <- ExtractVocabulary(D)
-
-        num_lines = num_lines + 1
-        if count < 152:
-            future_phrases.extend(line.rstrip('\n').split('\n'))
-        elif count > 151:
-            wise_phrases.extend(line.rstrip('\n').split('\n'))
-        count = count + 1
-    print(vocab)
-    print(len(vocab))
-    num_words = len(vocab) # N <- CountDocs(D)
-
-    # label info
-    num_wise = 0
-    num_future = 0
-
-    for line in train_labels:
-        if '0' in line:
-            num_wise = num_wise + 1
-        if '1' in line:
-            num_future = num_future + 1
-
-    for c in classes: # for each c in C
-        # for i in range(10):
-        #     print('X', '\n')
-        # do Nc <- CountDocsInClass(D, c)
-        Nc = 0 # number of phrases per class
-        if c == 0:
-            Nc = num_wise
-        elif c == 1:
-            Nc = num_future
-
-        prior_c.append(Nc/num_lines) # prior[c] <- Nc/N
-
-        textc = ''# textc <- ConcatenateTextOfAllDocsInClass(D, c)
-        if c == 0:
-            textc = ' '.join(str(phrase) for phrase in wise_phrases)
-        elif c == 1:
-            textc = ' '.join(str(phrase) for phrase in future_phrases)
-
-        Tct = 0
-        for term in vocab: # for each t in V
-            # do Tct <- CountTokensOfTerm(textc, t)
-            # Tct = number of occurrences of t in training docs from class c
-            if term in textc:
-                Tct = Tct + 1
+from argparse import ArgumentParser
 
 
-if __name__ == "__main__":
-    main()
+def getArgs():
+    '''Get the filepaths needed to run algo'''
+    parser = ArgumentParser()
+    parser.add_argument('--data', help='text file containing strings')
+    parser.add_argument('--labels', help='text file containing classes')
+    args = parser.parse_args()
+    return args.data, args.labels
+
+
+def fileToList(filename):
+    '''Helper function to turn text files to lists'''
+    return [line.rstrip('\n') for line in open(filename, 'r').readlines()]
+
+
+def mapAllDocs(data, labels):
+    '''Return a list of dictionaries containing mapped lines (docs) to classes
+
+       Each item in the list of the final output will be of the form:
+       {'doc': 'some string from text file', 'class': classIdentifier}
+    '''
+    # first turn both files into lists
+    docs = fileToList(data)
+    classes = fileToList(labels)
+
+    # they should be the same length
+    assert len(docs) == len(classes)
+
+    # create a final list that maps the two together into a dictionary
+    mapped = []
+    for d, c in zip(docs, classes):
+        temp = {'doc': d, 'class': c}
+        mapped.append(temp)
+
+    return mapped
+
+
+def getAllClasses(labels):
+    '''Returns a set of the unique classes from text file'''
+    return {c for c in fileToList(labels)}
+
+
+def extractVocabulary(D):
+    '''Returns a set of unique words from ALL docs'''
+    # brain is too dead to figure out how to set comp this
+    vocab = set()
+    for line in D:
+        for word in line['doc'].split():
+            vocab.add(word)
+
+    return vocab
+
+
+def countDocs(D):
+    '''Get TOTAL number of docs'''
+    return float(len(D))
+
+
+def filterOnClass(li, c):
+    '''Filter our big list by the class, return a filtered list'''
+    return [item for item in li if item['class'] == c]
+
+
+def countDocsInClass(D, c):
+    '''Return a count of the docs with the given class'''
+    return float(len(filterOnClass(D,c)))
+
+
+def concatenateTextOfAllDocsInClass(D, c):
+    '''Return a list of all the words in the given class'''
+    # extract all the docs so we can mush it all together
+    filtered = filterOnClass(D, c)
+    docs = [item['doc'] for item in filtered]
+
+    # @Maxi do we need to take just the unique words from here?
+    # Based on the algo I don't think so but it wouldn't be hard to change if we do.
+    words = []
+    for d in docs:
+        for word in d.split():
+            words.append(word)
+
+    return words
+
+
+def countTokensOfTerm(textc, t):
+    '''Givem a term, count how many times it appears in the text'''
+    return textc.count(t)
+
+def formula(Tct, t):
+    '''TODO IMPLEMENT THE FORMULA'''
+    # temp
+    import random
+    return random.randint(0,5)
+
+
+def trainMultinomialNB(C, D):
+    '''Honestly I'm not really sure what this section does, just following notes
+
+       C = all classes; c = one class
+        These are stored as a set
+       D = entire document; d = one line
+        These are stored in a list of dictionaries
+    '''
+    V = extractVocabulary(D)
+    N = countDocs(D)
+
+    # we need to define some vars for this loop
+    prior = [None] * len(C)   # define an empty list with length of num classes
+    condprob = {t:{} for t in V}    # define a dictionary of the terms with empty dict to be filled out
+
+    for c in C:
+        Nc = countDocsInClass(D, c)
+        prior[int(c)] = Nc/N
+        textc = concatenateTextOfAllDocsInClass(D, c)
+
+        for t in V:
+            Tct = countTokensOfTerm(textc, t)
+            condprob[t][c] = formula(Tct, t)    # todo
+
+    return V, prior, condprob
+
+
+def applyMultinomialNB(C, V, prior, condprob, d):
+    # I don't have the patiene for this right now
+    return 0
+
+
+if __name__ == '__main__':
+    # We need to do a little prep before running the algo
+    data, labels = getArgs()
+    D = mapAllDocs(data, labels)
+    C = getAllClasses(labels)
+
+
+    # run the first part of the algo
+    V, prior, condprob = trainMultinomialNB(C, D)
+
+    print('V: {}'.format(V))
+    print('prior: {}'.format(prior))
+    print('condprob: {}'.format(condprob))
